ca38b2f9b68a00d77241d38a640503e450e7dbd1
Edward Barcenas <edward.barcenas16@gmail.com>
2022-11-22 15:42:11 -0600

    2 files from hacker rank, stdout practice

---
+++ src/corejava_practice/Solution.java
@@ -0,0 +1,23 @@
+package corejava_practice;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+        
+        scan.close();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Learner_XZHCG238
+ *
+ */
+module corejava_practice_1122 {
+}
